31438bbae4822510efe436da65b66dcb46c6df6e
CharlieDelPrete <226cdelprete@frhsd.com>
2026-02-10 10:31:17 -0500

    fifth commit

--- NJIT-3_StarterFile-master/site.js
+++ NJIT-3_StarterFile-master/site.js
@@ -1,50 +1,29 @@
-/* SITE.JS: THIS FILE CONTAINS THE METHODS/FUNCTIONS AND VARIABLES CONTROLLING YOUR SITE
-//
-*/
-
-/* NOTE: MOVIES.JSON CONTAINS A LIST OF MOVIES AND ACCOMPANYING METADATA
-//
-//    They are in the following format:
-//    title (String): the name of the movie
-//    iscore (Number): the IMDB score
-//    rating (String): the movie's MPAA rating
-//    released (Array): the release date. Note that the order of the array is:  YYYY, MM, DD
-//    country (String): the country of production
-//    posters (Array): an array of String values with the URL to movie posters (in your img/ directory)
-//    imdb (String): the URL to the corresponding IMDB website
-//    website (String): the URL to the corresponding official website
-//    likes (Number): a fictitious number of user likes
-//    dislikes (Number): a fictitious number of user dislikes
-//    posterindex (Number): a counter to use with the "posters" array to keep track of the current displayed poster index
-//
-// FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
-*/
-
-
 const vue_app = Vue.createApp({
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-      data() {
-        return {
-            movies: [],
-            currentPosterIndex: 0
-         }
-      },
-      methods: {
-            nextPoster() {
-                  const posterCount = this.movies[this.currentPosterIndex]?.posters?.length || 0
-                  this.movies[this.currentPosterIndex].posterindex = 
-                        (this.movies[this.currentPosterIndex].posterindex + 1) % posterCount
-            },
-            previousPoster() {
-                  const posterCount = this.movies[this.currentPosterIndex]?.posters?.length || 0
-                  this.movies[this.currentPosterIndex].posterindex = 
-                        (this.movies[this.currentPosterIndex].posterindex - 1 + posterCount) % posterCount
-            }
-      }
+  data() {
+    return {
+      movies: []
+    }
+  },
+
+  created() {
+    fetch('movies.json')
+      .then(response => response.json())
+      .then(data => {
+        this.movies = data
+      })
+  },
+
+  methods: {
+    nextPoster(movie) {
+      const total = movie.posters.length
+      movie.posterindex = (movie.posterindex + 1) % total
+    },
+
+    previousPoster(movie) {
+      const total = movie.posters.length
+      movie.posterindex = (movie.posterindex - 1 + total) % total
+    }
+  }
 })
 
-vue_app.mount("#vue_app")
+vue_app.mount('#vue_app')
